08d1be2353a91e842cbf278e0fb710f515970f1a
dylenfu <dylenfu@126.com>
2020-05-26 09:58:08 +0800

    modify scripts

--- scripts/multi_node.sh
+++ scripts/multi_node.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#startHttpPort=10030
-#startNodePort=20030
-
 basedir=target/multi_nodes
 rm -rf $basedir
 mkdir $basedir
@@ -12,15 +9,5 @@ do
 workspace=$basedir/node${idx}
 mkdir $workspace $workspace/log
 cp target/node/* $workspace/
-
-#let httpPort=startHttpPort+$idx
-#let nodePort=startNodePort+$idx
-
-#cd $workspace
-#echo "$workspace running..."
-#./node -config=config.json \
-#	-log=log4go.xml \
-#	-httpport=$startHttpPort \
-#	-nodeport=$startNodePort
 
 done
