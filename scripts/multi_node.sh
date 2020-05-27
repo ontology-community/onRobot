cef7cfaae317bf7d1b73d09ebdbf54db2848c727
dylenfu <dylenfu@126.com>
2020-05-27 19:44:10 +0800

    modify scripts

--- scripts/multi_node.sh
+++ scripts/multi_node.sh
@@ -1,40 +1,33 @@
 #!/bin/sh
 
 # linux
-# ps -ef | grep p2pnode | grep -v grep | cut -c 9-15 | xargs kill -9
+ps -ef | grep p2pnode | grep -v grep | cut -c 9-15 | xargs kill -9
 # mac
-ps -ef | grep p2pnode | grep -v grep | cut -c 7-11 | xargs kill -9
+# ps -ef | grep p2pnode | grep -v grep | cut -c 7-11 | xargs kill -9
 
-basedir=/Users/dylen/workspace/gohome/src/github.com/ontology-community/onRobot
+basedir=/home/ubuntu/ontology/node/dht
 cd $basedir
-make build-node
 
-datadir=$basedir/target/node
-cd $datadir
-
-rm -rf log
-mkdir log
+datadir=$basedir/node
 rm -rf $basedir/p2pnode*
-mv node p2pnode
+cp $datadir/node $basedir/p2pnode
 
 startHttpPort=10000
 startNodePort=20000
 
-for idx in $(seq 1 20)
+for idx in $(seq 1 10)
 do
 
 name=p2pnode$idx
 httpPort=`expr $startHttpPort + $idx`
 nodePort=`expr $startNodePort + $idx`
-workspace=$datadir/$name
+workspace=$datadir
 echo "workspace $workspace, httpport $httpPort, nodeport $nodePort"
 
-mkdir $workspace
-mkdir $workspace/log
-cp $datadir/config.json $workspace/config.json
-cp $datadir/log4go.xml $workspace/log4go.xml
+#cp $datadir/config.json $workspace/config.json
+#cp $datadir/log4go.xml $workspace/log4go.xml
 
-nohup ./p2pnode -config=$workspace/config.json \
+sudo nohup ./p2pnode -config=$workspace/config.json \
 -log=$workspace/log4go.xml \
 -httpport=$httpPort \
 -nodeport=$nodePort &
